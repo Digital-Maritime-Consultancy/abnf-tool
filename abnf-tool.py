5fb56848079e4008a1afdc15dae6327f8922b2f9
Oliver Haagh <oliver@dmc.international>
2022-05-11 16:43:32 +0200

    Make it possible to pickle and upickle a lego object Also provided an example showing that MCP MRN is a proper subset of MRN

--- abnf-tool.py
+++ abnf-tool.py
@@ -1,7 +1,10 @@
+import pickle
+import re
+
 from abnf import Rule
 from abnf.grammars.misc import load_grammar_rules
+from abnf_to_regexp.single_regexp import translate, represent
 from greenery import lego
-from abnf_to_regexp.single_regexp import *
 
 urn_abnf_path = "urn-abnf.txt"
 
@@ -11,13 +14,16 @@ with open(urn_abnf_path, 'r') as f:
     if rulelist[-1] == '':
         rulelist = rulelist[:-1]
 
+
 @load_grammar_rules()
 class Urn(Rule):
     grammar = rulelist
 
+
 regex = translate(Urn('namestring'))
-urn_str = represent(regex).replace('\#', '#')
-urn = re.compile(urn_str)
+urn_re_str = represent(regex).replace('\#', '#').replace('{,', '{0,')
+print(urn_re_str)
+urn_re = re.compile(urn_re_str)
 
 mrn_abnf_path = "mrn-abnf.txt"
 
@@ -27,26 +33,44 @@ with open(mrn_abnf_path, 'r') as f:
     if rulelist[-1] == '':
         rulelist = rulelist[:-1]
 
+
+def parse_regex(regexp):
+    return lego.parse(regexp).reduce()
+
+
 @load_grammar_rules()
 class Mrn(Rule):
     grammar = rulelist
+
 
 # for rule in Mrn.rules():
 #     print(f"{rule.name} = {rule.definition}")
 
 regex = translate(Mrn('mrn'))
-r = represent(regex).replace('\#', '#')
-#print(r)
-mrn = re.compile(r)
+mrn_re_str = represent(regex).replace('\#', '#').replace('{,', '{0,')
+print(mrn_re_str)
+# mrn_re = re.compile(mrn_re_str)
+#
+# start = time.time()
 
-urn_fsm = lego.parse(urn_str).to_fsm()
-mrn_fsm = lego.parse(r).to_fsm()
+urn_fsm = parse_regex(urn_re_str)
+mrn_fsm = parse_regex(mrn_re_str)
+# end = time.time()
 
-if urn_fsm.ispropersuperset(mrn_fsm):
-    print("MRN is a subset of URN")
+# duration = end - start
+# print(f"Parsing took {duration:.2f} seconds")
+
+with open('urn_lego.bin', 'wb') as f:
+    pickle.dump(urn_fsm, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('mrn_lego.bin', 'wb') as f:
+    pickle.dump(mrn_fsm, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+#
+# if urn_fsm.ispropersuperset(mrn_fsm):
+#     print("MRN is a subset of URN")
 
 # test = 'urn:mrn:imo:imo-number:9743368'
-
 
 
 # match = re.match(mrn, test)
