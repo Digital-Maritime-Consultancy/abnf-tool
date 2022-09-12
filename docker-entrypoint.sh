d616edc21a643c9fb2dea229e550bb55041e45dd
Oliver Haagh <oliver@dmc.international>
2022-09-12 14:20:29 +0200

    Make it possible to configure DB connections for container using env variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,10 +17,48 @@
 set -eou pipefail
 MODE="$1"
 
+ARGS=()
+
+if [[ -v REDIS_HOST ]]; then
+  ARGS+=("--redis_host" "$REDIS_HOST")
+fi
+
+if [[ -v REDIS_PORT ]]; then
+  ARGS+=("--redis_port" "$REDIS_PORT")
+fi
+
+if [[ -v REDIS_DB ]]; then
+  ARGS+=("--redis_db" "$REDIS_DB")
+fi
+
+if [[ -v REDIS_PASSWORD ]]; then
+  ARGS+=("--redis_password" "$REDIS_PASSWORD")
+fi
+
+if [[ -v NEO4J_URI ]]; then
+  ARGS+=("--neo4j_uri" "$NEO4J_URI")
+fi
+
+if [[ -v NEO4J_USER ]]; then
+  ARGS+=("--neo4j_user" "$NEO4J_USER")
+fi
+
+if [[ -v NEO4J_PASSWORD ]]; then
+  ARGS+=("--neo4j_password" "$NEO4J_PASSWORD")
+fi
+
 if [[ $MODE == "server" ]]; then
-  exec python3 server.py
+  if [[ ${#ARGS[@]} -eq 0 ]]; then
+    exec python3 server.py
+  else
+    exec python3 server.py "${ARGS[@]}"
+  fi
 elif [[ $MODE == "bootstrap" ]]; then
-  exec python3 -u abnf-tool.py "${@:2}"
+  if [[ ${#ARGS[@]} -eq 0 ]]; then
+    exec python3 abnf-tool.py
+  else
+    exec python3 -u abnf-tool.py "${ARGS[@]}"
+  fi
 else
   exec "$@"
 fi
